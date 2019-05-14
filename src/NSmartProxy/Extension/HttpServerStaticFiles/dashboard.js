74cfb33e2feccb7b005886e2362644dbb9cf6115
shaohaozhi <shaohaozhi@live.cn>
2019-05-14 20:12:42 +0800

    dashboard chart.

--- src/NSmartProxy/Extension/HttpServerStaticFiles/dashboard.js
+++ src/NSmartProxy/Extension/HttpServerStaticFiles/dashboard.js
@@ -1,91 +1,121 @@
 /* globals Chart:false, feather:false */
 
-  (function () {
-  'use strict'
-  // Graphs
-  var ctx1 = document.getElementById('myChart')
-  var ctx2 = document.getElementById('myChart2')
-  // eslint-disable-next-line no-unused-vars
-  //仪表盘
-  var myChart = new Chart(ctx1, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday'
-       
-      ],
+(function() {
+    //'use strict'
+    // Graphs
+    var ctx1 = document.getElementById('myChart');
+    var ctx2 = document.getElementById('myChart2');
+    var ctx3 = document.getElementById('myChart3');
 
-      animationSteps: 15,
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff',
+    // eslint-disable-next-line no-unused-vars
+    //仪表盘
+    var myChart = new Chart(ctx1,
+        {
+            type: 'line',
+            data: {
+                labels: [
+                    '','','','', '', ''],
 
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
+                animationSteps: 15,
+                datasets: [
+                    {
+                        data: [
+                           
+                        ],
+                        lineTension: 0,
+                        backgroundColor: 'transparent',
+                        borderColor: '#007bff',
+                        borderWidth: 4,
+                        pointBackgroundColor: '#007bff',
 
-     
-      var myChart2 = new Chart(ctx2, {
-          type: 'bar',
-          data: {
-              labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-              datasets: [{
-                  label: '# of Votes',
-                  data: [12, 19, 3, 5, 2, 3],
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)'
-                  ],
-                  borderColor: [
-                      'rgba(255, 99, 132, 1)',
-                      'rgba(54, 162, 235, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)'
-                  ],
-                  borderWidth: 1
-              }]
-          },
-          options: {
-              scales: {
-                  yAxes: [{
-                      ticks: {
-                          beginAtZero: true
-                      }
-                  }]
-              }
-          }
-      });
-}())
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    yAxes: [
+                        {
+                            ticks: {
+                                beginAtZero: false
+                            }
+                        }
+                    ]
+                },
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: '连接'
+                }
+            }
+        });
+    
+
+    var myChart2 = new Chart(ctx2,
+        {
+            type: 'doughnut',
+            data: {
+                labels: ['NSmart内存', '其他应用内存'],
+                datasets: [
+                    {
+                        label: '内存占用',
+                        data: [12, 19],
+                        backgroundColor: [
+                            "rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"
+                          
+                        ]
+                        
+                        
+                    }
+                ]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: '内存'
+                }
+            }
+        });
+    var myChart3 = new Chart(ctx3,
+        {
+            type: 'doughnut',
+            data: {
+                labels: ['活跃用户', '离线用户'/*, 'Yellow', 'Green', 'Purple', 'Orange'*/],
+                datasets: [
+                    {
+                        label: '活跃用户',
+                        data: [12, 19],
+                        backgroundColor: [
+                            "rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"
+
+                        ]
+                        
+                    }
+                ]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: '在线用户'
+                }
+            }
+          
+        });
+
+    setInterval(function () {
+
+
+            //myChart2.data.datasets.pop();
+            //更新数据
+            myChart2.data.datasets[0].data[1] += 3;
+            //myChart2.data.datasets[1] = 10;
+            //myChart2.data.datasets.push({
+            //label: label,
+            // backgroundColor: color,
+            //  data: [12, 19]
+            //});
+            myChart2.update();
+        }, 1000
+    );
+}());
