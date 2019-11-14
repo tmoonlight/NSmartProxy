8f68b4ee4ba5a1b6d41410c4804d4453d1bf3ff4
tmoonlight <tmoonlight@163.com>
2019-11-14 15:56:11 +0800

    CI script updated.

--- src/build.cmd
+++ src/build.cmd
@@ -1,20 +1,36 @@
 rem windows only
 rem NSP v1.2
-@ECHO off
+rem @ECHO off
 
 set Ver=v1.2
 set BuildPath=%~dp0../build
 
-set nsp_server_path=%BuildPath%/nspclient_%Ver%
-set nsp_client_path=%BuildPath%/nspserver_%Ver%
+set nsp_client_path=%BuildPath%/nspclient_%Ver%
+set nsp_server_path=%BuildPath%/nspserver_%Ver%
+set nsp_client_scd_linux_path=%BuildPath%/nspclient_scd_linux_%Ver%
+set nsp_client_scd_win_path=%BuildPath%/nspclient_scd_win_%Ver%
+set nsp_client_scd_osx_path=%BuildPath%/nspclient_scd_osx_%Ver%
+set nsp_server_scd_linux_path=%BuildPath%/nspserver_scd_linux_%Ver%
+set nsp_server_scd_win_path=%BuildPath%/nspserver_win_%Ver%
+set nsp_server_scd_osx_path=%BuildPath%/nspserver_osx_%Ver%
 set nsp_client_winfform_path=%BuildPath%/nspclient_winform_%Ver%
 
 rem del %~dp0/../build/*.*
 rem NSPClient
-dotnet publish .\NSmartProxyClient\NSmartProxyClient.csproj -c release -o %nsp_server_path%
+dotnet publish .\NSmartProxyClient\NSmartProxyClient.csproj -c release -o %nsp_client_path%
 
 rem NSPServer
-dotnet publish .\NSmartProxy.ServerHost\NSmartProxy.ServerHost.csproj -c release -o %nsp_client_path%
+dotnet publish .\NSmartProxy.ServerHost\NSmartProxy.ServerHost.csproj -c release -o %nsp_server_path%
+
+rem NSPClient_SCD
+dotnet publish .\NSmartProxyClient\NSmartProxyClient.csproj -r linux-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -o %nsp_client_scd_linux_path%
+dotnet publish .\NSmartProxyClient\NSmartProxyClient.csproj -r win-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -o %nsp_client_scd_win_path%
+dotnet publish .\NSmartProxyClient\NSmartProxyClient.csproj -r osx-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -o %nsp_client_scd_osx_path%
+
+rem NSPServer_SCD
+dotnet publish .\NSmartProxy.ServerHost\NSmartProxy.ServerHost.csproj -r linux-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -o %nsp_server_scd_linux_path%
+dotnet publish .\NSmartProxy.ServerHost\NSmartProxy.ServerHost.csproj -r win-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -o %nsp_server_scd_win_path%
+dotnet publish .\NSmartProxy.ServerHost\NSmartProxy.ServerHost.csproj -r osx-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -o %nsp_server_scd_osx_path%
 
 rem NSPWinform
 MSBuild .\NSmartProxyWinform\NSmartProxyWinform.csproj /t:build /p:OutDir=%nsp_client_winfform_path%
@@ -25,8 +41,15 @@ rem ilmerge
 rem ruined :<
 
 rem compress
-powershell Compress-Archive -Path '%nsp_server_path%/*' -DestinationPath '%BuildPath%/nspclient_%Ver%.zip' -Force 
-powershell Compress-Archive -Path '%nsp_client_path%/*' -DestinationPath '%BuildPath%/nspserver_%Ver%.zip' -Force 
+powershell Compress-Archive -Path '%nsp_client_path%/*' -DestinationPath '%nsp_client_path%.zip' -Force 
+powershell Compress-Archive -Path '%nsp_server_path%/*' -DestinationPath '%nsp_server_path%.zip' -Force 
+powershell Compress-Archive -Path '%nsp_client_scd_linux_path%/*' -DestinationPath '%nsp_client_scd_linux_path%.zip' -Force 
+powershell Compress-Archive -Path '%nsp_client_scd_win_path%/*' -DestinationPath '%nsp_client_scd_win_path%.zip' -Force 
+powershell Compress-Archive -Path '%nsp_client_scd_osx_path%/*' -DestinationPath '%nsp_client_scd_osx_path%.zip' -Force 
+powershell Compress-Archive -Path '%nsp_server_scd_linux_path%/*' -DestinationPath '%nsp_server_scd_linux_path%.zip' -Force 
+powershell Compress-Archive -Path '%nsp_server_scd_win_path%/*' -DestinationPath '%nsp_server_scd_win_path%.zip' -Force 
+powershell Compress-Archive -Path '%nsp_server_scd_osx_path%/*' -DestinationPath '%nsp_server_scd_osx_path%.zip' -Force 
+
 powershell Compress-Archive -Path '%nsp_client_winfform_path%/*' -DestinationPath '%BuildPath%/nspclient_winform_%Ver%.zip' -Force 
 
 powershell explorer %~dp0..\build
